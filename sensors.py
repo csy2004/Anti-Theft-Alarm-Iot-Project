e13bd16e96dae3f021ce930224844eced6dffec4
CHAND SHEKHAR YADAV <139550239+csy2004@users.noreply.github.com>
2024-10-04 18:26:16 +0530

    Add files via upload

---
+++ sensors.py
@@ -0,0 +1,43 @@
+from onem2m import *
+
+uri_cse = "http://127.0.0.1:8080/~/in-cse/in-name"
+
+# ========================== room for PIR sensor =====================================
+
+ae1 = "Motion"
+cnt1 = "node1"
+
+create_ae(uri_cse, ae1)
+
+uri_ae1 = uri_cse + "/" + ae1
+create_cnt(uri_ae1, cnt1)
+
+uri_cnt1 = uri_ae1 + "/" + cnt1
+create_data_cin(uri_cnt1, "random_value")
+
+# =========================== room for temperature =====================================
+
+ae3 = "Temperature"
+cnt3 = "node3"
+
+create_ae(uri_cse, ae3)
+
+uri_ae3 = uri_cse + "/" + ae3
+create_cnt(uri_ae3, cnt3)
+
+uri_cnt3 = uri_ae3 + "/" + cnt3
+create_data_cin(uri_cnt3, "random_value")
+
+# =========================== room for humidity =====================================
+
+ae4 = "Humidity"
+cnt4 = "node4"
+
+create_ae(uri_cse, ae4)
+
+uri_ae4 = uri_cse + "/" + ae4
+create_cnt(uri_ae4, cnt4)
+
+uri_cnt4 = uri_ae4 + "/" + cnt4
+create_data_cin(uri_cnt4, "random_value")
+
